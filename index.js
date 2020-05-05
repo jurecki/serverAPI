90d428607a5b2ed2bd00199aad2465ad6b5be2d1
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-05-05 09:16:10 +0200

    Added frontent of ReactApp New Wave Festival

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const db = require('./db')
+const cors = require('cors')
 
 //import routes
 const testimonialsRoutes = require('./routes/testimodials.routes')
@@ -10,6 +11,7 @@ const seatsRoutes = require('./routes/seats.routes')
 //middleware to use for all request
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 app.use('/api', testimonialsRoutes); // add testimonialsRoutes routes to server
 app.use('/api', concertsRoutes);// add concertsRoutes routes to server
 app.use('/api', seatsRoutes);// add seatsRoutes routes to server
@@ -19,6 +21,6 @@ app.use((req, res) => {
     res.status(404).send({ message: 'Not found...' });
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port: 3000')
+app.listen(8000, () => {
+    console.log('Server is running on port: 8000')
 })

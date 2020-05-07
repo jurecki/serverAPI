4a2651cccc7088f395015b1ccfa56b2ddb94b4b9
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-05-07 21:29:08 +0200

    Deployed in heroku

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const db = require('./db')
 const cors = require('cors')
+const path = require('path')
 
 //import routes
 const testimonialsRoutes = require('./routes/testimodials.routes')
@@ -16,11 +17,20 @@ app.use('/api', testimonialsRoutes); // add testimonialsRoutes routes to server
 app.use('/api', concertsRoutes);// add concertsRoutes routes to server
 app.use('/api', seatsRoutes);// add seatsRoutes routes to server
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
+
 // catch error link
 app.use((req, res) => {
     res.status(404).send({ message: 'Not found...' });
 })
 
-app.listen(8000, () => {
-    console.log('Server is running on port: 8000')
-})
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
+});
